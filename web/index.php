468284ca58adfc32cc55ca7c9d61f45901350286
Abed Sujan <abedsujan@Abeds-MacBook-Pro.local>
2016-02-23 18:50:03 +0100

    Fix autoloader url

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'off');
 
 $app = new \MonoSolutions\Application();
 
